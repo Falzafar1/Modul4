8d84553c3e62a7b8e9e13097c8956fc88aeed6ea
Falzafar1 <161683671+Falzafar1@users.noreply.github.com>
2024-03-18 22:33:38 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,28 @@
-/** EL2208 Praktikum Pemecahan Masalah dengan C 2022/2023
- *   Modul               : Recursion
- *   Hari dan Tanggal    : Senin, 18 Maret 2024
- *   Nama (NIM)          : Naufal Afiq Muzaffar (13222025)
- *   Nama File           : main.c
- *   Deskripsi           : Menentukan apakah Koko Aang dan kekasihnya akan pergi dan di jam berapa bergantung dengan
- *                         pesan yang dikirim dengan menghitung panjang karakter pada kedua kalimat kata dan dilakukan
- *                         FPB kepada keduanya.  
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-int HitungKarakter(char *x){
+int HitungKarakter(){
+    char kata[100]="";
+    char kalimat[100]="";
+    char x[100]="";
+    int totalhuruf=0;
 
-    int totalangka=0;
-    int ambilangka=0;
-    char ParsedWords[8][100]={};
-    int count=0;
-    int j;
+    fgets(x, sizeof(x), stdin);
+    strcpy(kalimat, x);
+    strcpy(kata, strtok(x," "));
 
-    //Menghitung jumlah spasi  
-    for ( j = 0; j < strlen(x); j++){
-        if (x[j] == ' '){
-            count +=1;
+    while (strlen(kalimat)>1){
+        totalhuruf = totalhuruf*10 + strlen(kata);
+        memmove(kalimat, kalimat + strlen(kata)+1, strlen(kalimat + strlen(kata)) + 1);
+        if (strcmp(kalimat,"") != 0){
+            strcpy(kata, strtok(NULL," "));
         }
     }
-
-    //Memisahkan setiap kata pada kalimat 
-    strcpy(ParsedWords[0],strtok(x," "));
-    for (j = 0; j < count; j++){
-        strcpy(ParsedWords[j+1], strtok(NULL," "));
-    }
-
-    // Mendapatkan jumlah karakterpada kata
-    for (j = 0; j < count+1; j++){
-        ambilangka = strlen(ParsedWords[j]);
-        totalangka = totalangka*10 + ambilangka;
-    }
-    return totalangka;
+    return totalhuruf-1;
 }
 
-// Rekursi FPB 
 int FPB(int x, int y){
     if (y==0){
         return x;
@@ -52,31 +32,30 @@ int FPB(int x, int y){
     }
 }
 
-
 int main() {
-
     int i,j;
-    int x;
-    int hasilmod;
-    char pesan[100];
-
+    int jam, hasilkurang;
+    
     printf("Pesan Koko Ang: ");
-    fgets(pesan, sizeof(pesan), stdin);
-    i = HitungKarakter(pesan);
+    i= HitungKarakter();  
     printf("Balasan: ");
-    fgets(pesan, sizeof(pesan), stdin);
-    j = HitungKarakter(pesan);
+    j= HitungKarakter();
 
-    //FPB 
-    x = FPB(i-1,j-1);
+    jam = FPB (i, j);
+    hasilkurang = jam - 24;
 
-    hasilmod = x % 24;
+    if ((jam%24) <=24 && jam != 1 && i !=1 && j !=1){
+        if (hasilkurang > 0){
+            printf("Besok nonton jam %d skuy", (jam % 24));
+        }
+        else{
+            printf("Besok ke kebun binatang jam %d skuy", jam);
+        }
 
-    if (hasilmod <=24 && hasilmod != 1){
-        printf("Besok ke kebun binatang jam %d skuy", hasilmod);
     }
     else{
         printf("Ditolak nih\nPadam abangkuh :(");
     }
+    
     return 0;
 }
